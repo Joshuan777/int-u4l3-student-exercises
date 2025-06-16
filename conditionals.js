bf8d31ee7975c6a8bf1ff52a45b59cf182d381a9
Joshuan777 <Joshuanmirandagonzalez@gmail.com>
2025-06-16 12:31:40 -0700

    rereree

--- conditionals.js
+++ conditionals.js
@@ -22,6 +22,11 @@ englishButton.addEventListener("click", function() {
     //  - The answer should be "Mon".
     //  - If the user enters the correct answer, use .innerHTML to display the word "Correct!" in the ".english-message" div. 
     //  - If incorrect, display "Wrong! Try again."
+    if (englishInput ==="Mon"){
+        document.querySelector('.english-message').innerHTML = 'correct!';
+    }   else{
+            document.querySelector('.english-message').innerHTML = 'Wrong! Try again.';
+    }
     
 
 
@@ -41,7 +46,11 @@ mathButton.addEventListener("click", function() {
     //  - The answer should be 30.
     //  - If the user enters the correct answer, use .innerHTML to display the word "Yessss!" in the ".math-message" div. 
     //  - If incorrect, display "Wrong! Try again."
-    
+    if (mathInput ===30){
+        document.querySelector('.math-message').innerHTML = 'correct';
+    }   else{
+        document.querySelector('.math-message').innerHTML = 'Wrong! Try again.';
+    }
 
 });
 
@@ -54,22 +63,31 @@ ssButton.addEventListener("click", function() {
     //  - The answer should be "Albany".
     //  - If the user enters the correct answer, use .innerHTML to display the word "You got it!" in the ".ss-message" div. 
     //  - If incorrect, display "Womp, womp. Try again."
-
+     if (ssInput ==="Albany"){
+        document.querySelector('.ss-message').innerHTML = 'You got it';
+    }   else{
+        document.querySelector('.ss-message').innerHTML = 'Womp, womp. Try again.';
+    }
 
 
 });
 
 
 scienceButton.addEventListener("click", function() {
+    let scienceInput = document.querySelector('.science').value;
     // 5. Declare a variable that stores the value of the user input in the science section.
     //  - Look at previous lines of code to help you out.
-
 
     // 6. Write an if-else conditional statement.
     //  - Check if the value of the input variable is correct.
     //  - The answer should be "solid".
     //  - If the user enters the correct answer, use .innerHTML to display the word "Rockstar!" in the ".science-message" div. 
     //  - If incorrect, display "Sad. Try again."
+    if (scienceInput ==="solid"){
+        document.querySelector('.science-message').innerHTML = 'Rockstar!';
+    }   else{
+        document.querySelector('.science-message').innerHTML = 'Sad. Try again.';
+    }
 
 
 
